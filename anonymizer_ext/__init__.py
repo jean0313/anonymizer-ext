3d64f541b4cf50064f18b7068e0b63cd70940873
jean0313 <denggang80@gmail.com>
2024-08-15 11:16:07 +0800

    refact: restruct

--- anonymizer_ext/__init__.py
+++ anonymizer_ext/__init__.py
@@ -1,20 +1,4 @@
-from .phone_number import *
-
-from typing import List, Optional
-from presidio_analyzer import Pattern
-from langchain_experimental.data_anonymizer import PresidioAnonymizer
-from presidio_anonymizer.entities import OperatorConfig
-
-def init_anonymizer_sg_phone_number(anonymizer: PresidioAnonymizer, recognizer_patterns: Optional[List[Pattern]] = None):
-    anonymizer.add_recognizer(SgPhoneNumberRecognizer(recognizer_patterns))
-
-    operator = SgPhoneNumberOperator()
-    anonymizer.add_operators({
-            "SG_PHONE_NUMBER": OperatorConfig(
-                "custom", {"lambda": operator.fake_sg_phone_number}
-            )
-        })
-    pass
+from .phone_number import init_anonymizer_sg_phone_number
 
 __all__ = [
     "init_anonymizer_sg_phone_number",
--- anonymizer_ext/phone_number/__init__.py
+++ anonymizer_ext/phone_number/__init__.py
@@ -2,7 +2,25 @@
 from .sg_phone_number_recognizer import SgPhoneNumberRecognizer
 from .sg_phone_number_operator import SgPhoneNumberOperator
 
+from typing import List, Optional
+from presidio_analyzer import Pattern
+from langchain_experimental.data_anonymizer import PresidioAnonymizer
+from presidio_anonymizer.entities import OperatorConfig
+
+def init_anonymizer_sg_phone_number(anonymizer: PresidioAnonymizer, recognizer_patterns: Optional[List[Pattern]] = None):
+    anonymizer.add_recognizer(SgPhoneNumberRecognizer(recognizer_patterns))
+
+    operator = SgPhoneNumberOperator()
+    anonymizer.add_operators({
+            "SG_PHONE_NUMBER": OperatorConfig(
+                "custom", {"lambda": operator.fake_sg_phone_number}
+            )
+        })
+    pass
+
+
 __all__= [
     "SgPhoneNumberRecognizer",
-    "SgPhoneNumberOperator"
+    "SgPhoneNumberOperator",
+    "init_anonymizer_sg_phone_number",
 ]
